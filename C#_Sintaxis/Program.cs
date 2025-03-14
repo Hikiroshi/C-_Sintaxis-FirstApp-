3ce00f2015e463c56e1b54e512173f99a6d53350
dima <brawlst2007@gmail.com>
2025-03-14 21:27:56 +0300

    Классы данных и создание анкеты

--- C#_Sintaxis/Program.cs
+++ C#_Sintaxis/Program.cs
@@ -4,6 +4,13 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Привет,\n мир");
+        string MyName = "Roma";
+        byte MyAge = 17;
+        bool MyPet = false;
+        float MyShoeSize = 39;
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("My age in " + MyAge);
+        Console.WriteLine("Do I have a pat? " + MyPet);
+        Console.WriteLine("My shoe size is " + MyShoeSize); 
     }
 }
