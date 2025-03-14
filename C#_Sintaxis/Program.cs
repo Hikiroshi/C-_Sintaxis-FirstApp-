379dee6862a954a7b527499edd805c9a8737e9c2
dima <brawlst2007@gmail.com>
2025-03-14 21:30:37 +0300

    обратился к типу чтобы узнать его максимальное и минимальное значение

--- C#_Sintaxis/Program.cs
+++ C#_Sintaxis/Program.cs
@@ -4,13 +4,7 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        string MyName = "Roma";
-        byte MyAge = 17;
-        bool MyPet = false;
-        float MyShoeSize = 39;
-        Console.WriteLine("My name is " + MyName);
-        Console.WriteLine("My age in " + MyAge);
-        Console.WriteLine("Do I have a pat? " + MyPet);
-        Console.WriteLine("My shoe size is " + MyShoeSize); 
+        Console.WriteLine("IntMin {0} ", int.MinValue);
+        Console.WriteLine("IntMax {0} ", int.MaxValue);
     }
 }
